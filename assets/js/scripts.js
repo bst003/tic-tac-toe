1d66554f3477242c586eacb73066122a9e67aa9a
bst003 <bstanner33@yahoo.com>
2022-05-06 23:44:10 -0400

    Update project notes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,9 @@
 /*
 
-create a module for the game board (gameBoard)
-    use a multi-dimensional array to represent the game board cells
-    create a function to display the game board cells
-        loop through array items and get total length
-        add that many divs to #board
-        add x and y data attributes to board cells (can be later used to mark location)
-    create function that takes active payers marker and adds it to _boardArray on click
-
-create a factory function to create the players (Players)
-    must take in value for marker
-    must take in value for player name
-
-create a module to control the flow of the game (gameFlow)
-    start game on start button click
-    Create player objects from form
-        pass into gameBoard?
-    control reset game button
-
-
-Questions?
-    What module/function should control how marks are added to the boardArray?
-        leaning towards gameBoard as it houses the board element and boardArray
-        seems like gameFlow should be for higher level stuff(start, reset, enter names, etc);
-    How to check for winning values?
-    Active player needs to change on click
-        Does this mean that _setActivePlayer should be part of the gameBoard?
+TODO LIST
+    Add logic for tie games
+    Add buttons to play again or to reset to start screen
+    Add vs computer option
 
 */
 
